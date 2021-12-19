48b55bddbc0ff3c123bd2d268a7db87144ede919
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2021-12-19 12:44:53 +0300

    clickListener на кнопке

--- app/src/main/java/com/example/tokotlin/MainActivity.kt
+++ app/src/main/java/com/example/tokotlin/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.tokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.tokotlin.R
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Button>(R.id.button).setOnClickListener(object: View.OnClickListener{
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity,"Button pressed", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
