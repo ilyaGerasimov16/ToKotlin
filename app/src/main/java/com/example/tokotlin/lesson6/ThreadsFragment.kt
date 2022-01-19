eb11c33a8df32f6e1a52d5035330154b7c86eed6
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2022-01-19 21:57:44 +0300

    Урок 6. выполнение задачи в одном, зацикленном потоке. Избавились от продолжения выполнения уже ненужных задач при выходе из фрагмента до завершения их выполнения

--- app/src/main/java/com/example/tokotlin/lesson6/ThreadsFragment.kt
+++ app/src/main/java/com/example/tokotlin/lesson6/ThreadsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.tokotlin.lesson6
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.tokotlin.databinding.FragmentThreadsBinding
 
 class ThreadsFragment : Fragment(){
@@ -15,16 +18,35 @@ class ThreadsFragment : Fragment(){
         return _binding!!
     }
 
+    val myThread = MyThread()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.button.setOnClickListener{
-            Thread{
-                val result = startCalculations(2)
-                requireActivity().runOnUiThread{
-                    binding.textView.text = result
-                }
-            }.start()
 
+
+        myThread.start()
+
+        binding.button.setOnClickListener{
+            myThread.handler?.post{
+                val result = startCalculations(2)
+                activity?.let{ activity->
+                    Handler(Looper.getMainLooper()).post{
+                        binding.mainContainer.addView(TextView(activity).apply {
+                            text = result
+                        })
+                    }
+                }
+
+            }
+        }
+    }
+
+    class MyThread:Thread(){
+        var handler:Handler? = null
+        override fun run() {
+            Looper.prepare()
+            handler = Handler(Looper.myLooper()!!)
+            Looper.loop()
         }
     }
 
@@ -46,7 +68,9 @@ class ThreadsFragment : Fragment(){
     }
 
     override fun onDestroy() {
+        myThread.handler?.removeCallbacksAndMessages(null)
         super.onDestroy()
+
         _binding = null
     }
 
