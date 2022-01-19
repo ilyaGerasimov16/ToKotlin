551b10f18218f09e1aa56ac904d4cd9db981e1ce
Ilya Gerasimov <ilya.gerasimov.97@mail.ru>
2022-01-19 18:37:04 +0300

    Урок 5. ловим ошибки в блоке catch{}, закрываем соединение httpsURLConnection.disconnect(). Урок 5 закончен.

--- app/src/main/java/com/example/tokotlin/utils/WeatherLoader.kt
+++ app/src/main/java/com/example/tokotlin/utils/WeatherLoader.kt
@@ -7,6 +7,7 @@ import com.example.tokotlin.model.WeatherDTO
 import com.google.gson.Gson
 import java.io.BufferedReader
 import java.io.InputStreamReader
+import java.lang.Exception
 import java.net.URL
 import java.util.stream.Collectors
 import javax.net.ssl.HttpsURLConnection
@@ -16,19 +17,26 @@ class WeatherLoader (private val onWeatherLoaded: OnWeatherLoaded){
 
 
     fun loadWeather(lat:Double, lon:Double,){
+        lateinit var httpsURLConnection:HttpsURLConnection
         Thread{
-            val url = URL("https://api.weather.yandex.ru/v2/informers?lat=$lat&lon=$lon")
-            val httpsURLConnection = (url.openConnection() as HttpsURLConnection).apply {
-                requestMethod = "GET"
-                readTimeout = 2000
-                addRequestProperty(API_KEY, BuildConfig.WEATHER_API_KEY)
+            try {
+                val url = URL("https://api.weather.yandex.ru/v2/informers?lat=$lat&lon=$lon")
+                httpsURLConnection = (url.openConnection() as HttpsURLConnection).apply {
+                    requestMethod = "GET"
+                    readTimeout = 2000
+                    addRequestProperty(API_KEY, BuildConfig.WEATHER_API_KEY)
+                }
+                val bufferedReader = BufferedReader(InputStreamReader(httpsURLConnection.inputStream))
+                val weatherDTO:WeatherDTO? = Gson().fromJson(convertBufferToResult(bufferedReader), WeatherDTO::class.java )
+                Handler(Looper.getMainLooper()).post{
+                    onWeatherLoaded.onLoaded(weatherDTO)
+                }
+            } catch (e:Exception){
+                onWeatherLoaded.onFailed("Ошибка ${e.message}")
             }
-            val bufferedReader = BufferedReader(InputStreamReader(httpsURLConnection.inputStream))
-            val weatherDTO:WeatherDTO? = Gson().fromJson(convertBufferToResult(bufferedReader), WeatherDTO::class.java )
-            Handler(Looper.getMainLooper()).post{
-                onWeatherLoaded.onLoaded(weatherDTO)
+            finally {
+                httpsURLConnection.disconnect()
             }
-            onWeatherLoaded.onFailed("Ошибка")
         }.start()
     }
 
